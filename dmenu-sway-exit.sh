dea8dbf82fbd00b2127781263593ce957e5dc18f
Michał Kopeć <mkopec12@gmail.com>
2021-02-23 01:05:18 +0100

    Added a power menu script

---
+++ dmenu-sway-exit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# A script to close/lock a sway session using dmenu or a dmenu-compatible menu.
+
+menu="dmenu"
+prompt="power"
+
+while getopts "m:p:" opts; do
+	case "${opts}" in
+		"m")
+			menu=$OPTARG;;
+		"p")
+			prompt=$OPTARG;;
+	esac
+done
+shift "$((OPTIND-1))"
+
+options="Shut down\nSuspend\nReboot\nLog out\nReboot into Windows"
+
+option=$(echo -e $options | $menu -p $prompt)
+
+case $option in
+	"Shut down")
+		systemctl poweroff;;
+	"Suspend")
+		systemctl suspend;;
+	"Reboot")
+		systemctl reboot;;
+	"Log out")
+		swaymsg exit;;
+	"Reboot into Windows")
+		windows;;
+esac
